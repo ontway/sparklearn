9a52ec89f8ea8e7574c4142aae8a40f6ea47f014
duxiaofei <ontway@163.com>
2019-01-14 23:45:12 +0800

    20190114

--- src/main/java/com/dxf/App.java
+++ src/main/java/com/dxf/App.java
@@ -10,6 +10,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(System.getenv("SPARK_HOME"));
+        System.out.println(System.getenv("JARS"));
     }
 }
